430987d0c7e8af73e06ed2fe11d18fbe8f424b04
Charles Bartlett <ownageunit@gmail.com>
2022-10-22 16:10:18 -0400

    let's talk button done

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,4 +1,5 @@
 import "./Welcome.css";
+import { ReactComponent as ArrowIcon } from "../../Icons/arrow-up-right.svg";
 
 const Welcome = () => {
   return (
@@ -9,6 +10,10 @@ const Welcome = () => {
         Hello! I'm a full stack MERN developer with a focus on Frontend. I'm
         currently intersted in full time positions working with React.js!
       </p>
+      <button>
+        Lets Talk
+        <ArrowIcon />
+      </button>
     </div>
   );
 };
