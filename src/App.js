cf7532dd5f06b20a1061e03572fd8767e6f4944c
Charles Bartlett <ownageunit@gmail.com>
2022-10-22 15:53:20 -0400

    welcome basic done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import MobileNav from "./components/MobileNav/MobileNav";
+import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
       <div className="Wrapper">
         <MobileNav />
+        <Welcome />
       </div>
     </div>
   );
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,16 @@
+import "./Welcome.css";
+
+const Welcome = () => {
+  return (
+    <div className="Welcome">
+      <h1>CHARLES BARTLETT</h1>
+      <h3>Full Stack Software Engineer</h3>
+      <p>
+        Hello! I'm a full stack MERN developer with a focus on Frontend. I'm
+        currently intersted in full time positions working with React.js!
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
