c0a84807fe8266bb9c2b11199ddbc9ec381af0b4
Charles Bartlett <ownageunit@gmail.com>
2022-11-01 16:28:19 -0400

    added more projects, added hover effect where website and github icons turn primary color

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -10,6 +10,21 @@ const Projects = () => {
         website="https://skully-rogers.netlify.app/"
         github="https://github.com/CharBar3/skully-rogers"
       />
+      <SingleProject
+        title="BingeList"
+        website="https://bingelist-project.netlify.app/bingelist/"
+        github="https://github.com/CharBar3/bingelist-frontend"
+      />
+      <SingleProject
+        title="Workout Time"
+        website="hhttps://workout-time.herokuapp.com/dashboard"
+        github="https://github.com/CharBar3/workout-time"
+      />
+      <SingleProject
+        title="Guess that Rick & Morty Character!"
+        website="https://charbar3.github.io/Guess-that-Rick-Morty-Character/"
+        github="https://github.com/CharBar3/Guess-that-Rick-Morty-Character"
+      />
     </div>
   );
 };
